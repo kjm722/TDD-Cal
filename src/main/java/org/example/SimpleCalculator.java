64a5eac0be2d295fd15a5cdb2c188f5c5471b7d1
kjm722 <kjm722@naver.com>
2024-11-27 09:49:52 +0900

    TDD_Calc

--- src/main/java/org/example/SimpleCalculator.java
+++ src/main/java/org/example/SimpleCalculator.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class SimpleCalculator {
         public int run(int num1, int num2, int num3) {
-            return 60;
+            return ((num1 + num2) * num2 + -num3) * num3 / num2;
         }
 }
