2af1c87de84533a3fe13322f41a2b15d4977ccfd
kjm722 <kjm722@naver.com>
2024-11-27 09:46:13 +0900

    TDD_Calc

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -6,4 +6,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleCalculatorTest {
     SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+    @Test
+    @DisplayName("((3 + 5) * 5 + -10) * 10 / 5")
+    public void t1(){
+        int re = simpleCalculator.run(3,5,10);
+
+        assertThat(re).isEqualTo(80);
+    }
 }
