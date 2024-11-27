399f6d46cadb7b52fdf25b456edc4be6c2ef5e22
kjm722 <kjm722@naver.com>
2024-11-27 09:45:36 +0900

    TDD_Calc

---
+++ src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class SimpleCalculator {
+        public int run(int num1, int num2, int num3) {
+            return 60;
+        }
+}
---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,9 @@
+import org.example.SimpleCalculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleCalculatorTest {
+    SimpleCalculator simpleCalculator = new SimpleCalculator();
+}
